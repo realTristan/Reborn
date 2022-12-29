4ff34a3ca2a450fb6fbb7c166c3e526037e05d9c
tristan <heytristaann@gmail.com>
2022-12-28 19:40:49 -0500

    cleaned up

--- api/src/lib/discord.rs
+++ api/src/lib/discord.rs
@@ -1,37 +1,31 @@
-use actix_web::web;
 use super::endpoints::MessageBody;
-
+use actix_web::web;
 
 // Define the request client as a global variable
 lazy_static::lazy_static! {
-    static ref DISCORD_TOKEN: String = String::new();
     static ref CLIENT: reqwest::Client = reqwest::Client::new();
 }
+
+// Define the discord token as a global variable
+static DISCORD_TOKEN: &str = "";
 
 // Discord API Documentation
 // https://discord.com/developers/docs/resources/channel#create-message
 
-// The send_message() function is used to 
-// send a message to a discord channel
-//
-// Make sure to get the channel_id from the database
-//
-pub async fn send_message(channel: i64, body: web::Json<MessageBody>) -> reqwest::Response {
-    return http_post(
-        &format!("https://discord.com/api/v8/channels/{}/messages", channel), 
-        &vec![("content", "Hello, world!")]
-    ).await;
-}
-
-// the http_post() function is used to send an http
-// post request to the provided url
-async fn http_post(url: &str, params: &Vec<(&str, &str)>) -> reqwest::Response {
-    return match CLIENT.post(url)
+// The send_message() function is used to send the
+// anti-cheat data to the provided discord channel.
+// The data includes the users running programs, the
+// a screenshot of the users screen, the users hwid, etc.
+pub async fn send_message(channel: i64, body: web::Json<MessageBody>) -> Option<reqwest::Response> {
+    return match CLIENT.post(&format!("https://discord.com/api/v8/channels/{}/messages", channel))
         .header("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
-        .form(params)
+        .header("authorization", DISCORD_TOKEN)
+        .form(
+            &vec![("content", "Hello, world!")]
+        )
         .send().await 
     {
-        Ok(r) => r,
-        Err(e) => panic!("failed to request provided url. {:?}", e),
+        Ok(r) => Some(r),
+        Err(_) => None
     };
 }
